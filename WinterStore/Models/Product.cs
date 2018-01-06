55111f7659adb8838dc714fec14d1968469911d9
mathsfreak7 <mathsfreak7@gmail.com>
2018-01-06 21:51:01 +0100

    adding a product class

---
+++ WinterStore/Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WinterStore.Models
+{
+    public class Product
+    {
+        [Required]
+        public int ProductId { get; set; }
+
+        [Required]
+        [MinLength(2, ErrorMessage = "Name of the product must be 2 characters or more")]
+        [Display(Name = "Name of the product")]
+        public string ProductName { get; set; }
+
+        [Display(Name = "Price for 1 day")]
+        public double PriceFor1DayOfRent { get; set; }
+
+        [Required]
+        [Display(Name = "Number available in stock")]
+        public byte NumberInStock { get; set; }
+
+        [Required]
+        [Display(Name = "Is it currently rented")]
+        public bool IsCurrentlyRented { get; set; }
+
+        [Display(Name = "Date of rent")]
+        public DateTime DateOfRent { get; set; }
+
+        [Display(Name = "Date of return")]
+        public DateTime DateOfReturn { get; set; }
+
+        [Display(Name = "Number of days left to return")]
+        public int NumberOfDaysLeftToReturn { get; set; }
+
+    }
+}
