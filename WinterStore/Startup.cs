cb20740ac979c71e52b47102214ea0b6b37a404e
mathsfreak7 <mathsfreak7@gmail.com>
2018-01-06 21:18:18 +0100

    inital commit

---
+++ WinterStore/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WinterStore.Startup))]
+namespace WinterStore
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
