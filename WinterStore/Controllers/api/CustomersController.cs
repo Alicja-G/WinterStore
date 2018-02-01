f7a15329fc751caa0aff6d2f72056f493b3b4052
mathsfreak7 <mathsfreak7@gmail.com>
2018-02-01 22:41:53 +0100

    Adding index views and actionlinks for store, customers and rental registry

---
+++ WinterStore/Controllers/api/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WinterStore.Models;
+
+namespace WinterStore.Controllers.api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+    }
+}
